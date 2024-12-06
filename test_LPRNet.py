c363298c25e1488b00a781a9990523c9650ebdc7
Brian Buchsbaum <96887398+bdbux@users.noreply.github.com>
2024-12-06 15:34:02 -0500

    Update test_LPRNet.py

--- test_LPRNet.py
+++ test_LPRNet.py
@@ -49,7 +49,7 @@ def get_parser():
     parser.add_argument('--phase_train', default=False, type=bool, help='train or test phase flag.')
     parser.add_argument('--num_workers', default=2, type=int, help='Number of workers used in dataloading')
     parser.add_argument('--cuda', default=True, type=bool, help='Use cuda to train model')
-    parser.add_argument('--show', default=True, type=bool, help='show test image and its predict result or not.')
+    parser.add_argument('--show', default=False, type=bool, help='show test image and its predict result or not.')
     parser.add_argument('--pretrained_model', default='LPRNet_Pytorch/weights/Final_LPRNet_model.pth', help='pretrained base model')
 
     args = parser.parse_args()
