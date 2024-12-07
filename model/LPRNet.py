4c87370019b6b20e29775214347d17b590241613
Brian Buchsbaum <96887398+bdbux@users.noreply.github.com>
2024-12-07 02:24:03 -0500

    Update LPRNet.py

--- model/LPRNet.py
+++ model/LPRNet.py
@@ -66,11 +66,11 @@ class LPRNet(nn.Module):
         
         keep_features = list()
         for i, layer in enumerate(self.backbone.children()):
-            if isinstance(layer, nn.MaxPool3d):
-                x = x.unsqueeze(2)
+            # if isinstance(layer, nn.MaxPool3d):
+            #     x = x.unsqueeze(2)
             x = layer(x)
-            if isinstance(layer, nn.MaxPool3d):
-                x = x.squeeze(2)
+            # if isinstance(layer, nn.MaxPool3d):
+            #     x = x.squeeze(2)
             
             if i in [2, 6, 13, 22]: # [2, 4, 8, 11, 22]
                 keep_features.append(x)
