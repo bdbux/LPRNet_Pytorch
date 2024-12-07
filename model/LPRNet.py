c56cb4f37b34b4c0028a077161030ad5eac6765c
Brian Buchsbaum <96887398+bdbux@users.noreply.github.com>
2024-12-07 01:39:27 -0500

    Update LPRNet.py

--- model/LPRNet.py
+++ model/LPRNet.py
@@ -60,48 +60,35 @@ class LPRNet(nn.Module):
         # )
 
         self.backbone = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1), # 0
-            nn.BatchNorm2d(num_features=64),
-            nn.ReLU(),  # 2
-        
-            # Originally MaxPool3d(kernel_size=(1,3,3), stride=(1,1,1))
-            nn.MaxPool2d(kernel_size=(3, 3), stride=(1, 1)),
-            
-            small_basic_block(ch_in=64, ch_out=128),    # 4
-            nn.BatchNorm2d(num_features=128),
-            nn.ReLU(),  # 6
-        
-            # Originally MaxPool3d(kernel_size=(1,3,3), stride=(2,1,2))
-            nn.MaxPool2d(kernel_size=(3, 3), stride=(1, 2)),
-        
-            small_basic_block(ch_in=64, ch_out=256),   # 8
-            nn.BatchNorm2d(num_features=256),
-            nn.ReLU(),  # 10
-            small_basic_block(ch_in=256, ch_out=256),   # 11
-            nn.BatchNorm2d(num_features=256),   # 12
-            nn.ReLU(),
-        
-            # Originally MaxPool3d(kernel_size=(1,3,3), stride=(4,1,2))
-            nn.MaxPool2d(kernel_size=(3, 3), stride=(1, 2)),
-        
-            nn.Dropout(dropout_rate),
-            nn.Conv2d(in_channels=64, out_channels=256, kernel_size=(1, 4), stride=1),  # 16
-            nn.BatchNorm2d(num_features=256),
-            nn.ReLU(),  # 18
-            nn.Dropout(dropout_rate),
-            nn.Conv2d(in_channels=256, out_channels=class_num, kernel_size=(13, 1), stride=1), # 20
-            nn.BatchNorm2d(num_features=class_num),
-            nn.ReLU(),  # 22
-        )
+        nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1),
+        nn.BatchNorm2d(num_features=64),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(3, 3), stride=(1, 1)),
+    
+        small_basic_block(ch_in=64, ch_out=128),
+        nn.BatchNorm2d(num_features=128),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(3, 3), stride=(1, 2)),
+    
+        # After the first block, we have 128 channels, so update this line:
+        small_basic_block(ch_in=128, ch_out=256),
+        nn.BatchNorm2d(num_features=256),
+        nn.ReLU(),
+        small_basic_block(ch_in=256, ch_out=256),
+        nn.BatchNorm2d(num_features=256),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(3, 3), stride=(1, 2)),
+    
+        nn.Dropout(dropout_rate),
+        nn.Conv2d(in_channels=64, out_channels=256, kernel_size=(1, 4), stride=1),
+        nn.BatchNorm2d(num_features=256),
+        nn.ReLU(),
+        nn.Dropout(dropout_rate),
+        nn.Conv2d(in_channels=256, out_channels=class_num, kernel_size=(13, 1), stride=1),
+        nn.BatchNorm2d(num_features=class_num),
+        nn.ReLU(),
+    )
 
-        
-        self.container = nn.Sequential(
-            nn.Conv2d(in_channels=448+self.class_num, out_channels=self.class_num, kernel_size=(1, 1), stride=(1, 1)),
-            # nn.BatchNorm2d(num_features=self.class_num),
-            # nn.ReLU(),
-            # nn.Conv2d(in_channels=self.class_num, out_channels=self.lpr_max_len+1, kernel_size=3, stride=2),
-            # nn.ReLU(),
-        )
 
     def forward(self, x):
         x = self.quant(x)
